d14ed55ebf5a08f503bf061d14117c349f251061
matteocortezi <rm551692@fiap.com.br>
2023-09-18 21:23:44 -0300

    criando input de nome e descrição

--- src/routes/produtos/adicionar/index.jsx
+++ src/routes/produtos/adicionar/index.jsx
@@ -7,7 +7,19 @@ export default function AdicionarProduto(){
         <main>
             <h1>Adicionar Produto</h1>
             <form onSubmit={handleSubmit}>
-                
+                <fieldset>
+                    <legend>Novo Produto</legend>
+                    <div>
+                        <label htmlFor="idNome">Nome:</label>
+                        <input type="text" name='nome' id='idNome' />
+                    </div>
+                    <div>
+                        <label htmlFor="idDesc">Descrição:</label>
+                        <input type="text" name='desc' id='idDesc' />
+                    </div>
+                    
+                    <button type="submit">Adicionar</button>
+                </fieldset>
             </form>
         </main>
     )
