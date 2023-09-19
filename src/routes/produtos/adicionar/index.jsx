947aa220328735a9baa5bdaec5e3bafad72ec267
VitorMiranda11 <vitormiranda4321@outlook.com>
2023-09-18 21:29:04 -0300

    aplicando estilos no index

--- src/routes/produtos/adicionar/index.jsx
+++ src/routes/produtos/adicionar/index.jsx
@@ -4,28 +4,28 @@ export default function AdicionarProduto(){
     
 
     return(
-        <main>
-            <h1>Adicionar Produto</h1>
-            <form onSubmit={handleSubmit}>
-                <fieldset>
-                    <legend>Novo Produto</legend>
-                    <div>
-                        <label htmlFor="idNome">Nome:</label>
-                        <input type="text" name='nome' id='idNome' />
+        <main className={classes.centralizar}>
+            <h1 className={classes.titulo}>Adicionar Produto</h1>
+            <form className={classes.form} onSubmit={handleSubmit}>
+                <fieldset className={classes.fieldset}>
+                    <legend className={classes.legend}>Novo Produto</legend>
+                    <div className={classes.divInputs}>
+                        <label className={classes.label} htmlFor="idNome">Nome:</label>
+                        <input className={classes.input} type="text" name='nome' id='idNome' />
                     </div>
-                    <div>
-                        <label htmlFor="idDesc">Descrição:</label>
-                        <input type="text" name='desc' id='idDesc' />
+                    <div className={classes.divInputs}>
+                        <label className={classes.label} htmlFor="idDesc">Descrição:</label>
+                        <input className={classes.input} type="text" name='desc' id='idDesc' />
                     </div>
-                    <div>
-                        <label htmlFor="idPreco">Preço:</label>
-                        <input type="number" min="0" name='preco' id='idPreco' />
+                    <div className={classes.divInputs}>
+                        <label className={classes.label} htmlFor="idPreco">Preço:</label>
+                        <input className={classes.input} type="number" min="0" name='preco' id='idPreco' />
                     </div>
-                    <div>
-                        <label htmlFor="idImg">Imagem:</label>
-                        <input type="text" name='img' id='idImg' />
+                    <div className={classes.divInputs}>
+                        <label className={classes.label} htmlFor="idImg">Imagem:</label>
+                        <input className={classes.input} type="text" name='img' id='idImg' />
                     </div>
-                    <button type="submit">Adicionar</button>
+                    <button className={classes.button} type="submit">Adicionar</button>
                 </fieldset>
             </form>
         </main>
