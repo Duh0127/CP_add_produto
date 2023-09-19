28d36bf4f99c23153b3dcb64df4d9d2933ea597c
matteocortezi <rm551692@fiap.com.br>
2023-09-18 21:21:38 -0300

    criando estrutura do formulario

--- src/routes/produtos/adicionar/index.jsx
+++ src/routes/produtos/adicionar/index.jsx
@@ -5,7 +5,10 @@ export default function AdicionarProduto(){
 
     return(
         <main>
-            
+            <h1>Adicionar Produto</h1>
+            <form onSubmit={handleSubmit}>
+                
+            </form>
         </main>
     )
 }
